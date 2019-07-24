ef652a5e8bdf958388c9556e7188ff282c1857bb
mengpeng <pengmeng@yangqianguan.com>
2019-07-24 19:03:40 +0800

    common

---
+++ src/views/components/nsc-form/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-form ref="form">
+
+    </el-form>
+</template>
+
+<script>
+export default {
+    props: {
+        options: {
+            type: Object,
+            default: () => {}
+        },
+        data:{
+            type: Object,
+            default: () => {}
+        }
+    },
+    computed:{
+        form
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
